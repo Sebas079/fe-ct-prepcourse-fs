9680f9a694667a784c7a4227ed85983bf608cee0
SebasNaio <sebas.tmk@gmail.com>
2023-06-29 08:42:04 -0300

    Ejercicios M07

--- prueba.js
+++ prueba.js
@@ -1,17 +1,40 @@
-function agregarPropiedad(objeto, propiedad) {
-   // Recibirás un objeto por parámetro.
-   // Debes agregarle una propiedad con el nombre recibido por parámetro.
-   // Esta propiedad será igual al valor `null`.
-   // Retornar el objeto.
-   // Tu código:
-  objeto[propiedad] = null;
-  return objeto;
-
-}
-
-let objeto={
-   nombre:"sebas",
-   saludo: "hola"
-
-}
-invocarMetodo(objeto, "saludo")
+class Persona {
+      // Crea el constructor de la clase "Persona".
+      // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
+      // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
+      // sus valores.
+      constructor(nombre,apellido,edad,domicilio){
+         this.nombre = nombre;
+         this.apellido = apellido;
+         this.edad = edad;
+         this.domicilio = domicilio;
+         
+      }
+      detalle(){
+         return  {
+            nombre:this.nombre,
+           apellido: this.apellido,
+            edad: this.edad,
+           domicilio: this.domicilio
+        }
+      }
+   }
+   function agregarMetodo() {
+      // La función agrega un método "datos" a la clase "Persona".
+      // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
+      // Ejemplo: "Juan, 22 años".
+      // Tu código:
+      Persona.prototype.datos=function(){
+       return  `${persona.nombre}, ${persona.edad} años`;
+      }
+     // Persona.datos();
+     
+   
+     }
+   
+   let persona = new Persona("Joaqui","Del Pino",13,"Ballest");
+   console.log(persona.detalle())
+   agregarMetodo();
+  // const persona = new Persona('Juan', 'Pérez', 22, 'Saavedra 123');
+   //Persona.prototype.datos=()=>`${this.nombre}, ${this.edad} años`;
+   console.log(persona.datos())
